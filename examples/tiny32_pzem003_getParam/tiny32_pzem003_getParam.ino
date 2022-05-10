9eecb0b2a27cdbbaa91529b75dafdfb1c2252e04
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-05-10 16:14:52 +0700

    Rev1.1
    - Add Relay On, Show Library version
    - Add PZEM_003_begin

--- examples/tiny32_pzem003_getParam/tiny32_pzem003_getParam.ino
+++ examples/tiny32_pzem003_getParam/tiny32_pzem003_getParam.ino
@@ -4,7 +4,10 @@
  * Hardware     :     tiny32         
  * Author       :     Tenergy Innovation Co., Ltd.
  * Date         :     14/04/2022
- * Revision     :     1.0
+ * Revision     :     1.1
+ * Rev1.0       :     Origital
+ * Rev1.1       :     Add Relay On, Show Library version
+ *                    Add PZEM_003_begin
  * website      :     http://www.tenergyinnovation.co.th
  * Email        :     admin@innovation.co.th
  * TEL          :     +66 82-380-3299
@@ -24,7 +27,11 @@ void setup()
 {
   Serial.begin(115200);
   Serial.printf("\r\n**** tiny32_pzem003_getParam ****\r\n");
+  mcu.PZEM_003_begin(RXD3,TXD3);
+  mcu.Relay(1);
+  vTaskDelay(3000);
   mcu.buzzer_beep(2); //buzzer 2 beeps
+  mcu.library_version();
 }
 
 void loop()
